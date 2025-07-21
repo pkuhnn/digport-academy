0fa138e0f2d9832660a96856530c8b4084953a62
“Paloma <paloma@palomakuhn.com>
2025-07-21 15:46:24 -0300

    aula 5 - exercicios let const

--- conceitosBasicos/dadosUsuario.js
+++ conceitosBasicos/dadosUsuario.js
@@ -7,13 +7,14 @@ let imc = peso / (altura * altura);
 let anoAtual = 2025;
 const anoNascimento = anoAtual - idade;
 
-console.log(nome.toLocaleLowerCase())
-console.log(anoNascimento)
-console.log(imc)
-console.log(peso)
-console.log(idade)
-console.log(altura)
-console.log(anoAtual)
+//console.log(nome.toLocaleLowerCase())
+//console.log(anoNascimento)
+//console.log(imc)
+//console.log(peso)
+//console.log(idade)
+//console.log(altura)
+//console.log(anoAtual)
 
 console.log(`${nome} tem ${idade} anos, pesa ${peso}kg`);
 console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
+console.log(`${nome.split(" ")[0].toLowerCase()} ${sobrenome.toLowerCase()} nasceu em ${anoNascimento}.`);
